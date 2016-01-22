29d035ab096aff46dc8328c2132385f5bc29b849
Frazier Mork <fraziermork@gmail.com>
2016-01-21 21:57:29 -0800

    beginning to build div clock

--- app.js
+++ app.js
@@ -1,6 +1,71 @@
 $(document).ready(function(){
   var divClock = document.getElementById('divClock');
 
+  var clockFaceDivs = [];
+  clockFaceDivs[0] = [200, 63.5, 10, 10, 1];
+  clockFaceDivs[1] = [236.5, 100, 10, 10, 2];
+  clockFaceDivs[2] = [250, 150, 10, 10, 3];
+  clockFaceDivs[3] = [236.5, 200, 10, 10, 4];
+  clockFaceDivs[4] = [200, 236.5, 10, 10, 5];
+  clockFaceDivs[5] = [150, 250, 10, 10, 6];
+  clockFaceDivs[6] = [100, 236.5, 10, 10, 7];
+  clockFaceDivs[7] = [63.5, 200, 10, 10, 8];
+  clockFaceDivs[8] = [50, 150, 10, 10, 9];
+  clockFaceDivs[9] = [63.5, 100, 10, 10, 10];
+  clockFaceDivs[10] = [100, 63.5, 10, 10, 11];
+  clockFaceDivs[11] = [150, 50, 10, 10, 12];
+
+  for (var i = 0; i < clockFaceDivs.length; i++) {
+    console.log(clockFaceDivs[i][4]);
+    var idString = clockFaceDivs[i][4] + 'holder';
+    var noHolder = document.createElement('div');
+    noHolder.className = 'clockElement';
+    noHolder.id = idString;
+    noHolder.textContent = clockFaceDivs[i][4];
+    // noHolder.style.width = clockFaceDivs[i][2];
+    // noHolder.style.height = clockFaceDivs[i][3];
+    divClock.appendChild(noHolder);
+    $('#' + idString).css('left', clockFaceDivs[i][0] - (clockFaceDivs[i][2]/2) );
+    $('#' + idString).css('top', clockFaceDivs[i][1] - (clockFaceDivs[i][3]/2));
+    $('#' + idString).width(clockFaceDivs[i][2] );
+    $('#' + idString).height(clockFaceDivs[i][3] );
+  }
+  var secondHandArray = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110];
+  var secondHandArrayIds = [];
+  for (var i = 0; i < secondHandArray.length; i++){
+    idString = secondHandArray[i] + 'second';
+    var secondHandDiv = document.createElement('div');
+    var secondHandDiv.id = idString;
+    
+  }
+  var minuteHandArray = [0, 10, 20, 30, 40, 50, 60, 70, 80];
+
+  var hourHandArray = [0, 10, 20, 30, 40, 50];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -53,11 +118,6 @@ $(document).ready(function(){
     increment: 0.10472,
     seconds: 0,
 
-    moveSecondHand: function(){
-      console.log('move second hand');
-      secondHand.drawSecondHand();
-      secondHand.updateSecondHand();
-    },
     updateSecondHand: function(){
       secondHand.seconds++;
       secondHand.angle = (secondHand.angle + secondHand.increment);// % 6.2832;
@@ -76,10 +136,6 @@ $(document).ready(function(){
     increment: 0.010472,
     seconds: 0,
 
-    moveMinuteHand: function(){
-      minuteHand.updateMinuteHand();
-      minuteHand.drawMinuteHand();
-    },
     updateMinuteHand: function(){
       if (secondHand.seconds % 6 === 0){
         minuteHand.angle = (minuteHand.angle + minuteHand.increment);
@@ -100,10 +156,6 @@ $(document).ready(function(){
     increment: 0.010472,
     seconds: 0,
 
-    moveHourHand: function(){
-      hourHand.updateHourHand();
-      hourHand.drawHourHand();
-    },
     updateHourHand: function(){
       if (secondHand.seconds % 72 === 0){
         hourHand.angle = (hourHand.angle + hourHand.increment);
